432b6735dd9d31158294b702836ebc157b92a165
Aishvi Shroff <aishvi.0202@gmail.com>
2018-08-31 23:08:34 +1000

    First Commit

--- ShootingApp/ViewController.swift
+++ ShootingApp/ViewController.swift
@@ -10,71 +10,117 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+enum BoxBodyType : Int
+{
+    case bullet = 1
+    case barrier = 2
+}
 
+class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDelegate
+{
     @IBOutlet var sceneView: ARSCNView!
     
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        let box1 = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.red
+        box1.materials = [material]
         
-        // Set the scene to the view
+        let box1Node = SCNNode(geometry: box1)
+        box1Node.name = "Barrier1"
+        box1Node.position = SCNVector3(0,0,-0.5)
+        box1Node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        box1Node.physicsBody?.categoryBitMask = BoxBodyType.barrier.rawValue
+        
+        let box2Node = SCNNode(geometry: box1)
+        box2Node.name = "Barrier2"
+        box2Node.position = SCNVector3(-0.2,0,-0.5)
+        box2Node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        box2Node.physicsBody?.categoryBitMask = BoxBodyType.barrier.rawValue
+        
+        let box3Node = SCNNode(geometry: box1)
+        box3Node.name = "Barrier3"
+        box3Node.position = SCNVector3(0.2,0,-0.5)
+        box3Node.physicsBody = SCNPhysicsBody(type: .static, shape: nil)
+        box3Node.physicsBody?.categoryBitMask = BoxBodyType.barrier.rawValue
+        
+        scene.rootNode.addChildNode(box1Node)
+        scene.rootNode.addChildNode(box2Node)
+        scene.rootNode.addChildNode(box3Node)
+        
         sceneView.scene = scene
+        self.sceneView.scene.physicsWorld.contactDelegate = self
+        registerTapGestureRecognizer()
     }
     
-    override func viewWillAppear(_ animated: Bool) {
+    func physicsWorld(_ world: SCNPhysicsWorld, didBegin contact: SCNPhysicsContact)
+    {
+        var contactNode :SCNNode!
+        if contact.nodeA.name == "Bullet"
+        {
+            contactNode = contact.nodeB
+        }
+        else
+        {
+            contactNode = contact.nodeA
+        }
+    }
+    private func registerTapGestureRecognizer()
+    {
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(shootItems))
+        self.sceneView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func shootItems(recognizer :UITapGestureRecognizer)
+    {
+        guard let currentFrame = self.sceneView.session.currentFrame
+        else
+        {
+            return
+        }
+        
+        var translation = matrix_identity_float4x4
+        translation.columns.3.z = -0.3
+        
+        let box = SCNBox(width: 0.05, height: 0.05, length: 0.05, chamferRadius: 0)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.yellow
+        box.materials = [material]
+        
+        let boxNode = SCNNode(geometry: box)
+        boxNode.name = "Bullet"
+        boxNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        boxNode.physicsBody?.isAffectedByGravity = false
+        boxNode.physicsBody?.categoryBitMask = BoxBodyType.bullet.rawValue
+        boxNode.physicsBody?.contactTestBitMask = BoxBodyType.barrier.rawValue
+        boxNode.simdTransform = matrix_multiply(currentFrame.camera.transform, translation)
+        
+        let forceVector = SCNVector3(boxNode.worldFront.x * 2,boxNode.worldFront.y * 2, boxNode.worldFront.z * 2)
+        boxNode.physicsBody?.applyForce(forceVector, asImpulse: true)
+        self.sceneView.scene.rootNode.addChildNode(boxNode)
+        
+    }
+    override func viewWillAppear(_ animated: Bool)
+    {
         super.viewWillAppear(animated)
         
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
+    override func viewWillDisappear(_ animated: Bool)
+    {
         super.viewWillDisappear(animated)
-        
-        // Pause the view's session
+    
         sceneView.session.pause()
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
-    }
-
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
